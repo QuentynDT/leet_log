f2c83ef9a33710b4940745017feeef16befa733b
Quentyn <racecarrau@gmail.com>
2026-02-14 13:42:12 +0530

    695: Largest Island

--- medium/695/x.cpp
+++ medium/695/x.cpp
@@ -1,9 +1,21 @@
 #include "../../timer.h"
-
 class Solution {
 public:
     int maxAreaOfIsland(vector<vector<int>>& grid) {
-        
+        const int m = grid.size(), n = grid[0].size();
+        auto dfs = [&](this auto self, int i, int j) -> int {
+            if(i < 0 || i >= m || j < 0 || j >= n || grid[i][j] == 0) return 0;
+            grid[i][j] = 0;
+            return 1 + self(i, j + 1) + self(i, j - 1) + self(i + 1, j) + self(i - 1, j);
+        };
+        int ans = 0;
+        for(int i = 0; i < m; i++){
+            for(int j = 0; j < n; j++){
+                if(grid[i][j] == 0) continue;
+                ans = max(ans, dfs(i, j));
+            }
+        }
+        return ans;
     }
 };
 
@@ -33,7 +45,7 @@ void runTests(vector<token>& tokens){
 int main(){
     const int n = 1;
     vector<token> tokens(n);
-    tokens[0] = {};
+    tokens[0] = {{{0,0,1,0,0,0,0,1,0,0,0,0,0},{0,0,0,0,0,0,0,1,1,1,0,0,0},{0,1,1,0,1,0,0,0,0,0,0,0,0},{0,1,0,0,1,1,0,0,1,0,1,0,0},{0,1,0,0,1,1,0,0,1,1,1,0,0},{0,0,0,0,0,0,0,0,0,0,1,0,0},{0,0,0,0,0,0,0,1,1,1,0,0,0},{0,0,0,0,0,0,0,1,1,0,0,0,0}}, 6};
     auto start = high_resolution_clock::now();
     runTests(tokens);
     auto end = high_resolution_clock::now();
