fffe33c82ef07b000250799299f66bcd87a8bb7c
Quentyn <racecarrau@gmail.com>
2026-02-18 15:27:27 +0530

    3638: Max Balanced Shipments

--- medium/3638/x.cpp
+++ medium/3638/x.cpp
@@ -3,36 +3,17 @@
 class Solution {
 public:
     int maxBalancedShipments(vector<int>& weight) {
-        stack<int> st;
+        int first = weight[0], ans = 0;
         const int n = weight.size();
-        vector<int> next(n, -1);
-        for(int i = n - 1; i >= 0; i--){
-            while(!st.empty() && weight[st.top()] < weight[i]){
-                next[st.top()] = i;
-                st.pop();
+        for(int i = 1; i < n; i++){
+            if(weight[i] < first){
+                first = -1;
+                ans++;
+            } else if(first == -1){
+                first = weight[i];
             }
-            st.push(i);
         }
-        vector<int> dp(n + 1, 0);
-        for(int i = n - 1; i >= 0; i--){
-            if(next[i] != -1) {
-                dp[next[i]] = max(dp[next[i]], 1 + dp[i + 1]);
-            }
-            dp[i] = max(dp[i], dp[i + 1]);
-        }
-        // for(int i = 0; i < n; i++){
-        //     cout << weight[i] << ' ';
-        // }
-        // cout << '\n';
-        // for(int i = 0; i < n; i++){
-        //     cout << next[i] << ' ';
-        // }
-        // cout << '\n';
-        // for(int i = 0; i < n; i++){
-        //     cout << dp[i] << ' ';
-        // }
-        // cout << '\n';
-        return dp[0];
+        return ans;
     }
 };
 
