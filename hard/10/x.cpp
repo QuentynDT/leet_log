c7925bb185635cff880afe3ce453eccf8226ebb1
Quentyn <racecarrau@gmail.com>
2026-03-15 17:36:24 +0530

    10

---
+++ hard/10/x.cpp
@@ -0,0 +1,61 @@
+#include "../../timer.h"
+
+class Solution {
+public:
+    bool isMatch(string s, string p) {
+        const int m = s.size(), n = p.size();
+        auto match = [&](int i, int j) -> bool {
+            return s[i] == p[j] || p[j] == '.';
+        };
+        vector<vector<bool>> dp(m + 1, vector<bool>(n + 1));
+        dp[0][0] = true;
+        for (int j = 0; j < n; j++){
+            if (p[j] == '*' && dp[0][j - 1])
+                dp[0][j + 1] = true;
+        }
+        for(int i = 0; i < m; i++){
+            for(int j = 0; j < n; j++){
+                if(p[j] == '*'){
+                    dp[i + 1][j + 1] = dp[i + 1][j - 1] || match(i, j - 1) && dp[i][j + 1];
+                } else if(match(i, j)){
+                    dp[i + 1][j + 1] = dp[i][j];
+                }
+            }
+        }
+        return dp[m][n];
+    }
+};
+
+struct token {
+    string s;
+    string p;
+    bool ans;
+    bool res;
+};
+
+void handle(token& data){
+    Solution s;
+    data.res = s.isMatch(data.s, data.p);
+}
+
+void runTests(vector<token>& tokens){
+    cout << '\n';
+    for(token& t : tokens){
+        handle(t);
+        cout << "Output: ";
+        display(t.res);
+        cout << "\nExpected: ";
+        display(t.ans);
+        cout << "\n\n";
+    }
+}
+
+int main(){
+    const int n = 1;
+    vector<token> tokens;
+    tokens.push_back({"aa", "a", false});
+    auto start = high_resolution_clock::now();
+    runTests(tokens);
+    auto end = high_resolution_clock::now();
+    showRunTime(start, end);
+}
